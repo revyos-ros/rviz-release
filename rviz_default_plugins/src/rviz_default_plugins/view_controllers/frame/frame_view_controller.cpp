9bb9752e8f15d9e9161c27175a5ef6573a68efeb
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2025-06-23 08:45:18 +0000

    Imported upstream version '11.2.18' of 'upstream'

--- rviz_default_plugins/src/rviz_default_plugins/view_controllers/frame/frame_view_controller.cpp
+++ rviz_default_plugins/src/rviz_default_plugins/view_controllers/frame/frame_view_controller.cpp
@@ -63,16 +63,12 @@ static const Ogre::Quaternion ROBOT_TO_CAMERA_ROTATION =
   Ogre::Quaternion(Ogre::Radian(-Ogre::Math::HALF_PI), Ogre::Vector3::UNIT_Y) *
   Ogre::Quaternion(Ogre::Radian(-Ogre::Math::HALF_PI), Ogre::Vector3::UNIT_Z);
 
-static const float PITCH_LIMIT_LOW = -Ogre::Math::HALF_PI + 0.001f;
-static const float PITCH_LIMIT_HIGH = Ogre::Math::HALF_PI - 0.001f;
-
-static const Ogre::Vector3 DEFAULT_FPS_POSITION = Ogre::Vector3(5, 5, 10);
-
 FrameViewController::FrameViewController()
 {
-  axis_property_ = new rviz_common::properties::EnumProperty("Point towards", fmtAxis(6),
-                                    "Point the camera along the given axis of the frame.", this,
-                                    SLOT(changedAxis()));
+  axis_property_ = new rviz_common::properties::EnumProperty(
+    "Point towards", fmtAxis(6),
+    "Point the camera along the given axis of the frame.", this,
+    SLOT(changedAxis()));
   axis_property_->addOption(ANY_AXIS, -1);
 
   // x,y,z axes get integers from 1..6: +x, -x, +y, -y, +z, -z
@@ -81,7 +77,8 @@ FrameViewController::FrameViewController()
   }
   previous_axis_ = axis_property_->getOptionInt();
 
-  locked_property_ = new rviz_common::properties::BoolProperty("Lock Camera", false,
+  locked_property_ = new rviz_common::properties::BoolProperty(
+    "Lock Camera", false,
     "Lock camera in its current pose relative to the frame", this);
 }
 
